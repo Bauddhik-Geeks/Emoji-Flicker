99309d166ea05bf914160795b47f5588c0387307
rakshit234 <52824782+rakshit234@users.noreply.github.com>
2021-10-01 23:11:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ openface.addEventListener('click', () => {
         openface.classList.remove('active');
     }
 });
+
+const face1 = document.querySelector('.glasses');
+const face2 = document.querySelector('.surprise');
+
+
+//event listener
+face1.addEventListener('click', () => {
+    if(face2.classList.contains('surprise')){
+        face2.classList.add('active');
+        face1.classList.remove('active');
+    }
+});
+
+face2.addEventListener('click', () => {
+    if(face1.classList.contains('glasses')){
+        face1.classList.add('active');
+        face2.classList.remove('active');
+    }
+});
