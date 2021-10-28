89788f56036f07bff8f1c71e87749551566ae86b
Tanishq Singh Anand <anandtanishqs@gmail.com>
2021-10-28 12:11:11 +0530

    Cleaned code

--- script.js
+++ script.js
@@ -96,13 +96,11 @@ let emojis = [
 function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-console.log(emojis.length);
 const emojiHTML = document.getElementsByClassName("container__emoji")[0];
 let emojiIndex = 0;
 
 emojiHTML.addEventListener("click", () => {
   emojiIndex = getRndInteger(0, 93);
-  console.log("Clicked !");
   if (emojis[emojiIndex] === undefined) {
     emojiHTML.innerHTML = "😍";
   } else {
